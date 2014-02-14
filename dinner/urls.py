ddcbc306a04d61cbb82636d401f4661050a44041
WoLpH <Wolph@wol.ph>
2014-02-14 11:06:06 +0100

    added date specific view

--- dinner/urls.py
+++ dinner/urls.py
@@ -3,6 +3,6 @@ from django.conf import urls
 urlpatterns = urls.patterns(
     'dinner.views',
     urls.url(r'^$', 'index', name='index'),
-    urls.url(r'^remove/$', 'remove', name='remove'),
+    urls.url(r'^(?P<date>\d{4}-\d{2}-\d{2})/$', 'index', name='index'),
 )
 
